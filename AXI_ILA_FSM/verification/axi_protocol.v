2ec4e555abe8c2dcb9db97a6ace6758943c8d1d0
Huaixi Lu <51335367+HuaixiLu@users.noreply.github.com>
2019-12-01 20:14:06 -0500

    Update axi_protocol.v

--- AXI_ILA_FSM/verification/axi_protocol.v
+++ AXI_ILA_FSM/verification/axi_protocol.v
@@ -266,7 +266,7 @@ begin
     else begin
         case(b_state)
             WAIT:
-                if (w_state == COMMIT && axi_wlast == 1 && (bready_in || axi_bready)) begin
+                if (w_state == COMMIT && axi_wlast == 1 &&  axi_bready) begin
                     axi_bvalid <= 1'b1;
                     axi_bready <= 1'b1;
                     axi_bresp <= 2'b00;
