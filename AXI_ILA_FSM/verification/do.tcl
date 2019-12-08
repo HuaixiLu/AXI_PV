252346af891faab98132807c53e199ce1e9c2fcd
Huaixi Lu <51335367+HuaixiLu@users.noreply.github.com>
2019-12-07 21:23:03 -0500

    Update do.tcl

--- AXI_ILA_FSM/verification/do.tcl
+++ AXI_ILA_FSM/verification/do.tcl
@@ -10,4 +10,7 @@ clock clk
 reset rst
 
 assert -name awready_assert {(fsm_axi_awready == ila_axi_awready)}
+assert -name awvalid_assert {(fsm_axi_awvalid == ila_axi_awvalid)}
+assert -name wready_assert {(fsm_axi_wready == ila_axi_wready)}
+assert -name wvalid_assert {(fsm_axi_wvalid == ila_axi_wvalid)}
 
