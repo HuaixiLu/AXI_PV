43662578f91c88290c9d257f7f546d543dc9ebc2
Huaixi Lu <51335367+HuaixiLu@users.noreply.github.com>
2019-12-09 13:03:02 -0500

    Update do.tcl

--- AXI_ILA_FSM/verification/do.tcl
+++ AXI_ILA_FSM/verification/do.tcl
@@ -11,6 +11,8 @@ reset rst
 
 assert -name awready_assert {(fsm_axi_awready == ila_axi_awready)}
 assert -name awvalid_assert {(fsm_axi_awvalid == ila_axi_awvalid)}
-assert -name wready_assert {(fsm_axi_wready == ila_axi_wready)}
-assert -name wvalid_assert {(fsm_axi_wvalid == ila_axi_wvalid)}
+assert -name wready_assert  {(fsm_axi_wready  == ila_axi_wready)}
+assert -name wvalid_assert  {(fsm_axi_wvalid  == ila_axi_wvalid)}
+assert -name bready_assert  {(fsm_axi_bready  == ila_axi_bready)}
+assert -name bvalid_assert  {(fsm_axi_bvalid  == ila_axi_bvalid)}
 
